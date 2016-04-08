61867622ea97478412a40d5c321f6ed4b2de5de5
mikemarango <mike.marango@gmail.com>
2016-04-07 22:08:32 -0700

    made changes to app config

---
+++ ZzaDashboard/ZzaDashboard/ViewModels/CustomerListViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zza.Data;
+using ZzaDashboard.Services;
+
+namespace ZzaDashboard.ViewModels
+{
+    public class CustomerListViewModel
+    {
+        private ICustomersRepository _repo = new CustomersRepository();
+
+        public CustomerListViewModel()
+        {
+            if (DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
+                return;
+
+            Customers = new ObservableCollection<Customer>(_repo.GetCustomersAsync().Result);
+        }
+        public ObservableCollection<Customer> Customers { get; set; }
+    }
+}
